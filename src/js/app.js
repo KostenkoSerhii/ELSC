1c877cbffc9e50dd3395d8a074dcfac7f3ed00e2
Serhii <kso_132@mail.ru>
2017-01-19 17:56:53 +0400

    lang menu

--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,29 @@
 //=include lib/svgxuse.min.js
 
 sayHello();
+
+$(document).ready(function(){
+/*begin ready*/
+
+/*begin lang*/
+var cloneLang = $(".lang__list").clone(true);
+cloneLang.attr("class", "lang__list-clone");
+$(".header__top-line").prepend(cloneLang);
+
+
+$(".mobile-lang-wiev").on("click", function(){
+	$(".lang__list-clone").slideToggle();
+});
+$(".lang__list-clone .lang__item").on("click", function(){
+	var img = $(this).find("img").clone(true);
+	$(".mobile-lang-wiev").append(img);
+	$(".mobile-lang-wiev").find("img").first("img").remove();
+});
+
+/*end lang*/
+
+
+
+
+/*end ready*/
+});
